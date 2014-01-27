8776f5885360ccf1bd34a92ef1ad84182439e286
Tim Hütz <tim@huetz.biz>
2014-01-27 16:46:16 +0100

    Started to implement a class for showing a progress bar on the console

---
+++ src/timmilicious/ui/ProgressBar.cxx
@@ -0,0 +1,29 @@
+#include <timmilicious/ui/ProgressBar.hxx>
+using namespace timmilicious::ui;
+
+ProgressBar::ProgressBar() {
+	this->mCurrentProgress = 0.0f;
+	this->mMaxProgress = 100.0f;
+}
+
+ProgressBar::~ProgressBar() {
+	// nothing to do here
+}
+
+void ProgressBar::setProgress( const float progress, bool refresh ) throw( std::range_error ) {
+	if( progress > this->mMaxProgress || progress < 0.0f ) {
+		throw std::range_error( "The new progress must be between 0.0 and the max. value of the progress." );
+	}
+	this->mCurrentProgress = progress;
+	if( refresh ) {
+		this->updateProgress();
+	}
+}
+
+float ProgressBar::getProgress() const throw( ) {
+	return this->mCurrentProgress;
+}
+
+void ProgressBar::updateProgress() throw( ) {
+	// TODO: refresh the rendering
+}
---
+++ src/timmilicious/ui/ProgressBar.hxx
@@ -0,0 +1,30 @@
+#if !defined( __TIMMILICIOUS_UI_PROGRESSBAR_HXX__ )
+	#define __TIMMILICIOUS_UI_PROGRESSBAR_HXX__
+
+//
+#include <stdexcept>
+
+namespace timmilicious {
+
+	namespace ui {
+
+		class ProgressBar {
+			public:
+				ProgressBar();
+				virtual ~ProgressBar();
+
+				void updateProgress() throw( );
+				float getProgress() const throw( );
+				void setProgress( const float progress, bool refresh = false ) throw( std::range_error );
+
+			private:
+				float mMaxProgress;
+				float mCurrentProgress;
+
+		}; /* class ProgressBar */
+
+	} /* namespace ui */
+
+} /* namespace timmilicious*/
+
+#endif /* if !defined( __TIMMILICIOUS_UI_PROGRESSBAR_HXX__ ) */
