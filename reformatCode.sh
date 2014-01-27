eabf60f2e8fedb4d3c477083d68c43bc5893b713
Tim Hütz <tim@huetz.biz>
2014-01-27 16:46:04 +0100

    Added code formatting rules

---
+++ reformatCode.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+find src/ -name "*.cxx" -o -name "*.hxx" -o -name "*.c" -o -name "*.h" -o -name "*.hxx.in" > fileList.txt
+uncrustify -c uncrustify.config -F fileList.txt --replace
+find src/ -name "*.unc-backup~" -o -name "*.unc-backup.md5~" > fileList.txt
+xargs -I{} sh -c 'rm {}' < fileList.txt
+rm fileList.txt
