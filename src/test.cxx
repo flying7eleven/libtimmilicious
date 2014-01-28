46d24c9c328e87ce473d375e5415b654693017ee
Tim Hütz <tim@huetz.biz>
2014-01-28 10:13:50 +0100

    Updated the test for the progress bar

--- src/test.cxx
+++ src/test.cxx
@@ -4,7 +4,9 @@ using namespace timmilicious::ui;
 int main( int, char ** ) {
 	ProgressBar *testBar = new ProgressBar();
 
-	testBar->updateProgress();
+	testBar->setStatusText( "Processing images..." );
+	testBar->setMaxProgress( 1000 );
+	testBar->setProgress( 0 );
 
 	delete testBar;
 	return 0;
