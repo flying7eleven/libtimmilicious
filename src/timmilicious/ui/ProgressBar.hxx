c9122bac043920fc6700fdf3837360a0f9dfcd6c
Tim Hütz <tim@huetz.biz>
2014-01-29 14:24:15 +0100

    Removed some empty lines from the code documentation

--- src/timmilicious/ui/ProgressBar.hxx
+++ src/timmilicious/ui/ProgressBar.hxx
@@ -20,7 +20,7 @@ namespace timmilicious {
 				 * \brief Default constructor of this class.
 				 */
 				ProgressBar() throw( );
-			
+
 				/**
 				 * \brief Default destructor of this class.
 				 */
@@ -32,22 +32,22 @@ namespace timmilicious {
 				 * \param[in] status The text to display.
 				 */
 				void setStatusText( const std::string & status ) throw( );
-			
+
 				/**
 				 * Redraw the progress line with the current state of the progress bar.
 				 */
 				void updateProgress() throw( );
-			
+
 				/**
 				 * Get the current progress.
 				 *
 				 * Get the current progress of the progress bar. This is *NOT* a percent value.
 				 * its the value stored by setProgress(...).
-				 * 
+				 *
 				 * \return The value for the current process.
 				 */
 				unsigned int getProgress() const throw( );
-			
+
 				/**
 				 * Set the current progress.
 				 *
@@ -57,7 +57,7 @@ namespace timmilicious {
 				 * \throws std::range_error Will be thrown if the value is higher than the max. value for the progress.
 				 */
 				void setProgress( const unsigned int progress, bool refresh = false ) throw( std::range_error );
-			
+
 				/**
 				 * Set the maximum value for the progress.
 				 *
@@ -66,7 +66,7 @@ namespace timmilicious {
 				 * \throws std::range_error Will be thrown if the new maximum value is lower than the current progress value.
 				 */
 				void setMaxProgress( const unsigned int max ) throw( std::range_error );
-			
+
 				/**
 				 * Get the current max. progress value.
 				 *
@@ -75,7 +75,7 @@ namespace timmilicious {
 				unsigned int getMaxProgress() const throw( );
 
 			private:
-			
+
 				/**
 				 * Get the current width of the terminal.
 				 *
@@ -85,9 +85,9 @@ namespace timmilicious {
 				 */
 				unsigned short int getTerminalWidth( int fileDescriptor ) const throw( );
 
-				std::string mStatusText; //<<< The current status text to use.
-				unsigned int mMaxProgress; //<<< The currently set max. progress value.
-				unsigned int mCurrentProgress; //<<< The current progress value.
+				std::string mStatusText; // <<< The current status text to use.
+				unsigned int mMaxProgress; // <<< The currently set max. progress value.
+				unsigned int mCurrentProgress; // <<< The current progress value.
 
 		}; /* class ProgressBar */
 
