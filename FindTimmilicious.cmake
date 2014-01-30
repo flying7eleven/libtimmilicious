5642fbbb85c16e78db0cc91d31589e394f06e7e0
Tim Hütz <tim@huetz.biz>
2014-01-30 08:56:03 +0100

    Rewrote most of the find script

--- FindTimmilicious.cmake
+++ FindTimmilicious.cmake
@@ -1,40 +1,39 @@
-# Locate registration
+# - Try to find Timmilicious
+# Once done this will define
 #
-# This module defines
-# TIMMILICIOUS_FOUND: if false, do not try to link to registration
-# TIMMILICIOUS_LIBRARY, TIMMILICIOUS_LIBRARIES, TIMMILICIOUS_LIBS: where to find libtimmilicious
-# TIMMILICIOUS_INCLUDE_DIR, TIMMILICIOUS_INCLUDE_DIRS, TIMMILICIOUS_INCLUDE_PATH: where to find the libtimmilicious header
-#
+#  TIMMILICIOUS_FOUND - system has libtimmilicious
+#  TIMMILICIOUS_INCLUDE_DIR - the libtimmilicious include directory
+#  TIMMILICIOUS_LIBRARIES - Link these to use libtimmilicious
 
-set(_INCLUDEDIRS ENV CPLUS_INCLUDE_PATH)
-set(_LIBDIRS ENV LD_LIBRARY_PATH)
+# find the libtimmilicious include directory
+FIND_PATH( _INCLUDE_DIR timmilicious/timmilicious.hxx
+		PATHS
+			/usr/local/include/
+			/usr/include/
+			/opt/include/
+			/opt/local/include/
+			$ENV{CPLUS_INCLUDE_PATH}
+	)
 
-# find the registration include directory
-find_path(_INCLUDE_DIR timmilicious/timmilicious.hxx
-PATHS
-/usr/local/include/
-/usr/include/
-/opt/include/
-/opt/local/include/
-${_INCLUDEDIRS}
-)
+# find the libtimmilicious library
+FIND_LIBRARY( _LIBRARIES 
+		NAMES
+			timmilicious
+		PATH_SUFFIXES
+			lib64 lib
+		PATHS 
+			/usr/local
+			/usr
+			/opt/local
+			/opt
+		$ENV{LD_LIBRARY_PATH}
+	)
 
-# find the registration library
-find_library(TIMMILICIOUS_LIBRARY 
-NAMES timmilicious
-PATH_SUFFIXES lib64 lib
-PATHS 
-/usr/local
-/usr
-/opt/local
-/opt
-${_LIBDIRS}
-)
-SET ( TIMMILICIOUS_LIBS ${TIMMILICIOUS_LIBRARY} )
-SET ( TIMMILICIOUS_LIBRARIES ${TIMMILICIOUS_LIBRARY} )
-SET ( TIMMILICIOUS_INCLUDE_DIRS ${TIMMILICIOUS_INCLUDE_DIR} )
-SET ( TIMMILICIOUS_INCLUDE_PATH ${TIMMILICIOUS_INCLUDE_DIR} )
+# set the globally used variables
+SET( TIMMILICIOUS_INCLUDE_DIR ${_INCLUDE_DIR} )
+SET( TIMMILICIOUS_LIBRARY ${_LIBRARIES} )
+MARK_AS_ADVANCED( TIMMILICIOUS_INCLUDE_DIR TIMMILICIOUS_LIBRARY )
 
-# handle the QUIETLY and REQUIRED arguments and set REGISTRATION_FOUND to TRUE if all listed variables are TRUE
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(TIMMILICIOUS DEFAULT_MSG TIMMILICIOUS_INCLUDE_DIR TIMMILICIOUS_LIBRARY)
+# handle the QUIETLY and REQUIRED arguments and set TIMMILICIOUS_FOUND to TRUE if all listed variables are TRUE
+INCLUDE( FindPackageHandleStandardArgs )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( TIMMILICIOUS DEFAULT_MSG TIMMILICIOUS_INCLUDE_DIR TIMMILICIOUS_LIBRARY )
