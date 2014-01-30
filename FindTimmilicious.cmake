b244499c261c7b9243f1576691dcc00135e0e711
Tim Hütz <tim@huetz.biz>
2014-01-30 10:52:14 +0100

    Simplified the find script

--- FindTimmilicious.cmake
+++ FindTimmilicious.cmake
@@ -6,17 +6,18 @@
 #  TIMMILICIOUS_LIBRARIES - Link these to use libtimmilicious
 
 # find the libtimmilicious include directory
-FIND_PATH( _INCLUDE_DIR timmilicious/timmilicious.hxx
+FIND_PATH( TIMMILICIOUS_INCLUDE_DIR timmilicious/timmilicious.hxx
 		PATHS
 			/usr/local/include/
 			/usr/include/
 			/opt/include/
 			/opt/local/include/
+			${TIMMILICIOUS_DIR}/include
 			$ENV{CPLUS_INCLUDE_PATH}
 	)
 
 # find the libtimmilicious library
-FIND_LIBRARY( _LIBRARIES 
+FIND_LIBRARY( TIMMILICIOUS_LIBRARIES 
 		NAMES
 			timmilicious
 		PATH_SUFFIXES
@@ -26,14 +27,10 @@ FIND_LIBRARY( _LIBRARIES
 			/usr
 			/opt/local
 			/opt
-		$ENV{LD_LIBRARY_PATH}
+			${TIMMILICIOUS_DIR}/lib
+			$ENV{LD_LIBRARY_PATH}
 	)
-
-# set the globally used variables
-SET( TIMMILICIOUS_INCLUDE_DIR ${_INCLUDE_DIR} )
-SET( TIMMILICIOUS_LIBRARY ${_LIBRARIES} )
-MARK_AS_ADVANCED( TIMMILICIOUS_INCLUDE_DIR TIMMILICIOUS_LIBRARY )
 
 # handle the QUIETLY and REQUIRED arguments and set TIMMILICIOUS_FOUND to TRUE if all listed variables are TRUE
 INCLUDE( FindPackageHandleStandardArgs )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( TIMMILICIOUS DEFAULT_MSG TIMMILICIOUS_INCLUDE_DIR TIMMILICIOUS_LIBRARY )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( TIMMILICIOUS DEFAULT_MSG TIMMILICIOUS_INCLUDE_DIR TIMMILICIOUS_LIBRARIES )
