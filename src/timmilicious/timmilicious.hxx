15b9b0eaa8ef9e59b2886f6d7c7d25101bbf6b13
Tim Hütz <tim@huetz.biz>
2014-01-30 08:28:58 +0100

    Added a core header for the library

---
+++ src/timmilicious/timmilicious.hxx
@@ -0,0 +1,6 @@
+#if !defined( __TIMMILICIOUS_TIMMILICIOUS_HXX__ )
+	#define __TIMMILICIOUS_TIMMILICIOUS_HXX__
+
+namespace timmilicious {} /* namespace timmilicious*/
+
+#endif /* if !defined( __TIMMILICIOUS_TIMMILICIOUS_HXX__ ) */
