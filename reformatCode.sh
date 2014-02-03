d8601f0479bf443635a0a653f6f074189dac333d
Tim Hütz <tim@huetz.biz>
2014-02-03 16:52:47 +0100

    Added *.cxx.in files to the rules for reformatting

--- reformatCode.sh
+++ reformatCode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find src/ -name "*.cxx" -o -name "*.hxx" -o -name "*.c" -o -name "*.h" -o -name "*.hxx.in" > fileList.txt
+find src/ -name "*.cxx.in" -o -name "*.cxx" -o -name "*.hxx" -o -name "*.c" -o -name "*.h" -o -name "*.hxx.in" > fileList.txt
 uncrustify -c uncrustify.config -F fileList.txt --replace
 find src/ -name "*.unc-backup~" -o -name "*.unc-backup.md5~" > fileList.txt
 xargs -I{} sh -c 'rm {}' < fileList.txt
