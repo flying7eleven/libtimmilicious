220372f0c0dcc4d4384e6154650ceb7d80b7e98d
Tim Hütz <tim@huetz.biz>
2014-02-03 16:52:18 +0100

    Added two method to query the API and the ABI version of the used library

--- src/timmilicious/timmilicious.hxx
+++ src/timmilicious/timmilicious.hxx
@@ -1,6 +1,26 @@
 #if !defined( __TIMMILICIOUS_TIMMILICIOUS_HXX__ )
 	#define __TIMMILICIOUS_TIMMILICIOUS_HXX__
 
-namespace timmilicious {} /* namespace timmilicious*/
+namespace timmilicious {
+
+	/**
+	 * Get the ABI version of the library.
+	 *
+	 * \returns The ABI version for the library.
+	 *
+	 * \warning The method is *NOT* thread-safe.
+	 */
+	const unsigned short int getABIVersion();
+
+	/**
+	 * Get the API verison of the library.
+	 *
+	 * \returns The API version for the library.
+	 *
+	 * \warning The method is *NOT* thread-safe.
+	 */
+	const char *getAPIVersion();
+
+} /* namespace timmilicious*/
 
 #endif /* if !defined( __TIMMILICIOUS_TIMMILICIOUS_HXX__ ) */
