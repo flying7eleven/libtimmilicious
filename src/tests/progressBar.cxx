e7381da0aa4ff4551afcd202150b8127945928ea
Tim Hütz <tim@huetz.biz>
2014-04-15 13:37:28 +0200

    Fixed some test cases

--- src/tests/progressBar.cxx
+++ src/tests/progressBar.cxx
@@ -10,6 +10,6 @@ TEST( ProgressBar, Constructor ) {
 	ASSERT_NO_THROW( ProgressBar( "This is a Test", 0 ) );
 	ASSERT_NO_THROW( ProgressBar( "", 10 ) );
 	ASSERT_NO_THROW( ProgressBar( "This is a Test", 10 ) );
-	ASSERT_NO_THROW( ProgressBar( "", -10 ) );
-	ASSERT_NO_THROW( ProgressBar( "This is a Test", -10 ) );
+	ASSERT_THROW( ProgressBar( "", -10 ), std::invalid_argument );
+	ASSERT_THROW( ProgressBar( "This is a Test", -10 ), std::invalid_argument );
 }
