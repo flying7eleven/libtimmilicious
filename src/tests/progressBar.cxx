303aa9c95fd314e71b2395278456c5a8d192f953
Tim Hütz <tim@huetz.biz>
2014-04-15 13:16:43 +0200

    Added the first test for the ProgressBar class

---
+++ src/tests/progressBar.cxx
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include <timmilicious/ui/ProgressBar.hxx>
+using namespace timmilicious::ui;
+
+TEST( ProgressBar, Constructor ) {
+	ASSERT_NO_THROW( ProgressBar() );
+	ASSERT_NO_THROW( ProgressBar( "" ) );
+	ASSERT_NO_THROW( ProgressBar( "This is a Test" ) );
+	ASSERT_NO_THROW( ProgressBar( "", 0 ) );
+	ASSERT_NO_THROW( ProgressBar( "This is a Test", 0 ) );
+	ASSERT_NO_THROW( ProgressBar( "", 10 ) );
+	ASSERT_NO_THROW( ProgressBar( "This is a Test", 10 ) );
+	ASSERT_NO_THROW( ProgressBar( "", -10 ) );
+	ASSERT_NO_THROW( ProgressBar( "This is a Test", -10 ) );
+}
