8e64d03a618d37cc76097a34895031b581c947de
Tim Hütz <tim@huetz.biz>
2014-04-15 17:27:29 +0200

    Changed the license comments to LGPL

--- src/tests/progressBar.cxx
+++ src/tests/progressBar.cxx
@@ -1,3 +1,20 @@
+/**
+ * This file is part of libTimmilicious.
+ * Copyright (C) 2014 Tim Hütz
+ *
+ * libTimmilicious is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libTimmilicious is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * ERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libTimmilicious. If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <gtest/gtest.h>
 #include <timmilicious/ui/ProgressBar.hxx>
 using namespace timmilicious::ui;
--- src/timmilicious/timmilicious.hxx
+++ src/timmilicious/timmilicious.hxx
@@ -1,19 +1,19 @@
 /**
- * libtimmilicious - A library for beautifying your console output
+ * This file is part of libTimmilicious.
  * Copyright (C) 2014 Tim Hütz
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * libTimmilicious is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * libTimmilicious is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * ERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see {http://www.gnu.org/licenses/}.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libTimmilicious. If not, see <http://www.gnu.org/licenses/>.
  */
 #if !defined( __TIMMILICIOUS_TIMMILICIOUS_HXX__ )
 	#define __TIMMILICIOUS_TIMMILICIOUS_HXX__
